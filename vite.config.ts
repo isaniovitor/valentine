ef77de4d43d54af475975ff40caf55369932561b
isaniovitor <isaniovitot@gmail.com>
2026-02-11 23:16:59 -0300

    ci: deploy pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine-2026/',
-  envPrefix: ['VITE_', 'EMAILJS_'],
+  base: "/valentine/",
+  envPrefix: ["VITE_", "EMAILJS_"],
   server: {
     port: 5173,
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: false,
   },
-})
+});
